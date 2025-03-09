a67f3354d07cc5feaabdc74a1c22f0ae704551b5
Kirilko-frontend <kkir614@gmail.com>
2025-03-09 14:28:12 +0100

    update build

---
+++ src/js/1-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from 'flatpickr/dist/flatpickr.js';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+startButton.addEventListener('click', () => {
+  if (!userSelectedDate) return;
+
+  startButton.disabled = true;
+  input.disabled = true;
+
+  timerId = setInterval(() => {
+    const now = new Date();
+    const timeLeft = userSelectedDate - now;
+    if (timeLeft <= 0) {
+      clearInterval(timerId);
+      updateTimerDisplay(0, 0, 0, 0);
+      input.disabled = false;
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    updateTimerDisplay(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  return {
+    days: Math.floor(ms / day),
+    hours: Math.floor((ms % day) / hour),
+    minutes: Math.floor(((ms % day) % hour) / minute),
+    seconds: Math.floor((((ms % day) % hour) % minute) / second),
+  };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
